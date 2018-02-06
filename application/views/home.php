da92e0e33dddcd64b67ca6a6982050b9486f1a3a
rsAccel27 <unknownkriture@yahoo.com>
2018-02-06 19:32:50 +0800

    preliminary login modal

--- application/views/home.php
+++ application/views/home.php
@@ -16,8 +16,43 @@
 				<a class="logo" href="home.html">Home</a>
 				<a class="logo" href="">About Us</a>
 				<a class="logo" href="">Contact Us</a>
-				<a class="logo" href="">Login</a>
+				<style> 
+					<?php include 'style.css'; ?> 
+				</style>
+				<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
 
+				<div id="id01" class="modal">
+  
+					<form class="modal-content animate" action="/action_page.php">
+						<div class="container">
+						  <label><b>Username</b></label>
+						  <input type="text" placeholder="Enter Username" name="uname" required>
+
+						  <label><b>Password</b></label>
+						  <input type="password" placeholder="Enter Password" name="psw" required>
+							
+						  <button type="submit">Login</button>
+						  <input type="checkbox" id="check" checked>
+						  <label for="check">Remember me</label>
+						</div>
+
+						<div class="container" style="background-color:#f1f1f1">
+						  <label class="psw">Forgot <a href="#">password?</a></label>
+						</div>
+					</form>
+				</div>
+
+			<script>
+			// Get the modal
+			var modal = document.getElementById('id01');
+
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+				if (event.target == modal) {
+					modal.style.display = "none";
+				}
+			}
+			</script>
 			</header>
 
 		<!-- Banner -->
