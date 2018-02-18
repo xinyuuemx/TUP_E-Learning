2b2c70bf7a6c16d7e6f41208ce5629eea0ca8bda
Your Name <throyembudo@gmail.com>
2018-02-18 14:23:57 +0800

    about_us

---
+++ application/views/about us .php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>About Us</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="<?php echo base_url().'/assets/css/main.css';?>" />
+		<link rel="stylesheet" href="<?php echo base_url().'/assets/css/login.css';?>" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<a class="logo" href="">Home</a>
+				<a class="logo" href="">About Us</a>
+				<a class="logo" href="">Contact Us</a>
+			</header>
+
+	
+
+
+		<!-- CTA -->
+		
+			<section  class="wrapper">
+			
+				<div class="inner">
+				<div style="width: 50%; float:left">
+					<img src="<?php echo base_url().'/assets/images/tup.png';?>" alt=""> 
+					<h1 align="center"><b><font color=#c41e3a>TUP HISTORY</font></b></h1><br/></br/></br/>
+					
+					<h2>MTS (1901-1910) </h2> 
+					<p>he Technological University of the Philippines was first established as the Manila Trade School in 1901 upon the enactment of Act No. 74 by the United States Philippine Commission for the instruction of the Filipinos on useful trades. Originally housed near Ateneo de Manila in Intramuros, MTS transferred to the Exposicion Regional de Filipinos in Calle Padre Faura. Six years later, the MTS transferred to the City Shops in Calle Arroceros. Industrial courses which included carpentry, English, telegraphy, machine shop work, furniture making, wood carving, boat building, plumbing, blacksmithing, mechanical and architectural drawing were offered on the primary school level during this period because pupils at that time were already adults. They were studying certain trades which they intended to follow after leaving school.</p>
+					<h2>AT THE HELM THROUGH A HUNDRED YEARS OF LEADERSHIP AND EXCELLENCE IN TECHNOLOGY EDUCATION</h2>
+					<p>Ronald P. Gleason was the first principal (1901-1904) of the trade school; J.J. Eaton was the first superintendent (1905-1906); and Gregorio J. Sevilla was the first Filipino Superintendent (1936-1939). Prof. Jose S. Roldan became the first president of PCAT (1959-1961) and Prof. Apolinario G. Apilado succeeded him as the second president (1962-1967). Dr. Jose R. Vergara was the third president of PCAT and the first president of TUP (1968-1988). He was succeeded by Dr. Frederick So. Pada, the second TUP president (1988-2000). Dr. Bernardo F. Adiviso served as third president (Aug. 1-16, 2000) followed by Dr. Fedeserio C. Camarao who became the fourth and centennial president (March 28,2001-March 27, 2005). Dr. Godofredo E. Gallega served as the fifth president (October 10, 2005-October 9, 2009); Dr. Olympio V. Caparas is the sixth president (October 16, 2009 to April 2014); and Dr. Adora S. Pili is the seventh and first woman president of TUP.
+
+						With more than a hundred years of existence since its establishment as MTS, TUP has indeed transformed its "trade school" image into a dynamic premier state university in the fields of industrial, technological and engineering education. Its mark of excellence is not only known locally but also internationally.
+
+					</p>
+				</div>
+				</div>
+				<div class="box1">
+					<div style="width: 50%; float:right">
+					<img src="<?php echo base_url().'/assets/images/tup.png';?>" alt=""> 
+					<h2 align="center"><b><font color=#c41e3a>VISION, MISSION, CORE VALUES, STRATEGIC GOALS</font></b></h2><br/>
+					
+					<h2>Vision</h2> 
+					<p> A premier state university with recognized excellence in engineering and technology education at par with leading universities in the ASEAN region.
+					</p>
+					
+					
+					<h2>Mission</h2> 
+					<p> The mission of TUP is stated in Section 2 of P.D. No. 1518 as follows:
+						The University shall provide higher and advanced vocational, technical, industrial, technological and professional education and training in industries and technology, and in practical arts leading to certificates, diplomas and degrees. It shall provide progressive leadership in applied research, developmental studies in technical, industrial, and technological fields and production using indigenous materials; effect technology transfer in the countryside; and assist in the development of small-and-medium scale industries in identified growth centers.
+					</p>
+					
+					
+					<h2>Core Values</h2> 
+					<p> <font color=#c41e3a>❇</font><b>T</b> - Transparent and participatory governance<br/>
+						<font color=#c41e3a>❇</font><b>U</b> - Unity in the pursuit of TUP mission, goals, and objectives<br/>
+						<font color=#c41e3a>❇</font><b>P</b> - Professionalism in the discharge of quality service<br/>
+						<font color=#c41e3a>❇</font><b>I</b> - Integrity and commitment to maintain the good name of the University<br/>
+						<font color=#c41e3a>❇</font><b>A</b> - Accountability for individual and organizational quality performance<br/>
+						<font color=#c41e3a>❇</font><b>N</b> - Nationalism through tangible contribution to the rapid economic growth of the country<br/>
+						<font color=#c41e3a>❇</font><b>S</b> - Shared responsibility, hardwork, and resourcefulness in compliance to the mandates of the university<br/>
+					</p>
+					
+					
+					<h2>STRATEGIC GOALS</h2> 
+					<p> <b> Goal 1 </b> - Quality & Responsive Curricular Offerings<br/>
+						<b> Goal 2 </b> - Excellence in Engineering & Technology Research<br/>
+						<b> Goal 3 </b> - Leadership in Community Services<br/>
+						<b> Goal 4 </b> - Strengthening Capability & Competence<br/>
+						<b> Goal 5 </b>- Modernized University System & Efficient Management of Resources<br/>
+						<b> Goal 6 </b> - Increased Financial Viability<br/>
+						<b> Goal 7 </b> - Enhanced Network & Sustained Collaboration Initiatives<br/>
+					</p>
+					
+					
+					<h2>QUALITY POLICY</h2> 
+					<p> TUP shall commit to provide quality higher and advanced education; conduct relevant research and extension projects; continually improve its value to customers through enhancement of personnel competence and effective quality management system compliant to statutory and regulatory requirements.
+					</p>
+				
+				</div>
+				</div>
+
+</section>
+		<!-- Scripts -->
+
+
+			<script src="<?php echo base_url().'/assets/js/jquery.min.js';?>"></script>
+			<script src="<?php echo base_url().'/assets/js/browser.min.js';?>"></script>
+			<script src="<?php echo base_url().'/assets/js/breakpoints.min.js';?>"></script>
+			<script src="<?php echo base_url().'/assets/js/util.js';?>"></script>
+			<script src="<?php echo base_url().'/assets/js/main.js';?>"></script>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	</body>
+</html>
