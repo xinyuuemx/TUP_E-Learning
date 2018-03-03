3f4f126052bc8d6e667c4b9ea9663018c4365171
SJouzu <you@example.com>
2018-03-03 16:17:50 +0800

    Fix indentions and remove unnecessary comments

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,38 +1,38 @@
 <?php if (!defined('BASEPATH')) exit ('No direct script access allowed');
+
 class Pages extends CI_Controller {
 		public function __construct(){
-		parent::__construct();
-		$this->load->helper('form');		
-		//pre load all models
-		$this->load->model('pages_model','pages');
+				parent::__construct();
+				$this->load->helper('form');
+				$this->load->model('pages_model','pages');
 		}
-        public function index(){
-			$this->load->view('home');
-        }
-        public function view(){
 
-              if ( ! file_exists(APPPATH.'views/home.php')){
-                      // Whoops, we don't have a page for that!
-                      show_404();
-              }
-              $this->load->view('home');
-        }
-		public function login_authorize(){	
-			$result = $this->pages->read_users($_POST['uname'],$_POST['psw']);
-			if(!empty($result)){
+		public function index(){
+				$this->load->view('home');
+    }
 
-			foreach($result as $mom){
-				//echo $mom['emp_name'];
-				$usernameee['uname']=$mom['username'];
-			}
-			echo $usernameee['uname'];
-			//$this->load->view('dashboard',$usernameee);
-			}	
-			else{
+		public function view(){
+        if ( ! file_exists(APPPATH.'views/home.php')){
+        		show_404();
+        }
+        $this->load->view('home');
+    }
+
+		public function login_authorize(){
+				$result = $this->pages->read_users($_POST['uname'],$_POST['psw']);
+				if(!empty($result)){
+						foreach($result as $mom){
+						//echo $mom['emp_name'];
+						$usernameee['uname']=$mom['username'];
+						}
+						echo $usernameee['uname'];
+						//$this->load->view('dashboard',$usernameee);
+				}	else {
 				$data['msg']='<font color=red>Invalid username and/or password.</font><br />';
 				$data['username']=$_POST['uname'];
 				$data['password']=$_POST['psw'];
 				$this->load->view('home');
-			}
-}
+				}
+		}
+
 }
