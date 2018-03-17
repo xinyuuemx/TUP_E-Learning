bf1868351a8fc4ca18ce48abf9ea2d181f5707c5
xinyuuemx <xinyuu.emx@gmail.com>
2018-03-17 09:05:12 +0800

    added prof dashboard header and added webpage title on homepage header

---
+++ application/views/template/prof_dashboard_header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>TUP E-LEARNING</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="<?php echo base_url().'/assets/css/main.css';?>" />
+		<link rel="stylesheet" href="<?php echo base_url().'/assets/css/login.css';?>" />
+	</head>
+
+	<body class="is-preload">
+		<nav>
+			<header id="header">
+				<div class="topnav" style="margin-left:5%;">
+					<a class="logo" href="<?php echo base_url().'professor'?>"><i class="icon fa-university" style="margin-right:5px;">&nbsp;</i>TUP E-LEARNING WEBSITE</a>
+				</div>
+				<div class="topnav" style="margin-right:6px;">
+						<a  href="<?php echo base_url().'Prof_dashboard/logout'?>">Sign Out</a>
+				</div>
+			</header>
+		</nav>
+
+		<div class="sidenav">
+			<?php $image=$img_id;?>
+			<img src="<?php echo base_url().'assets/images/'.$image.'.jpg';?>" alt="Profile Picture" width="240" height="240">
+
+			<ul class="alt">
+				<li><b><?php $name_print = $name; echo $name_print;?></b></li>
+				<li><?php $pid = $prof_id; echo $pid;?></li>
+			</ul>
+
+			<ul class="alt">
+				<li><a href="<?php echo base_url().'professor';?>">Dashboard</a></li>
+				<li><a href="<?php echo base_url().'Prof_dashboard/index/classes';?>">Classes</a></li>
+				<li><a href="#">Discussions</a></li>
+				<li><a href="<?php echo base_url().'Prof_dashboard/index/modules';?>">Modules</a></li>
+			</ul>
+		</div>
+	</body>
